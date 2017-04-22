1cef87ed5d76cff86c76c823d59b56fa28f77cb4
Deepakkv <deepakkv@in-deepakkv.local>
2017-04-22 15:56:28 +0530

    <T1> [Deepak] Test cases for ElasticSearchClient with Valid and Invalid BaseURLs

--- config/config.go
+++ config/config.go
@@ -2,6 +2,7 @@ package config
 
 const (
 	ElasticSearchBaseURL = "http://localhost:9200/"
+	InvalidElasticSearchBaseURL = "http://remotehost:9200/"
 	IndexName = "esspike"
 	TypeName = "log"
 	IndexMapping = ``
--- elasticsearch/ping_test.go
+++ elasticsearch/ping_test.go
@@ -1,1 +1,18 @@
 package elasticsearch
+
+import (
+	"testing"
+	"github.com/stretchr/testify/assert"
+	"elasticsearch-spike/config"
+)
+
+func TestNewElasticSearchClient(t *testing.T) {
+	esClient := NewElasticSearchClient(config.ElasticSearchBaseURL)
+	assert.NotNil(t, esClient)
+}
+
+func TestNewElasticSearchClientPanicOnUnreachableURl(t *testing.T) {
+	assert.Panics(t, func() {
+		NewElasticSearchClient(config.InvalidElasticSearchBaseURL)
+	}, "NewElasticSeachClient should panic on Invalid URL.")
+}
